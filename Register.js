2ff1748de7dbccf9a3bed2ef18b914c1859f6a26
hananeelaouad <85617562+hananeelaouad@users.noreply.github.com>
2021-06-23 21:31:16 +0100

    Update Register.js

--- Register.js
+++ Register.js
@@ -6,47 +6,51 @@ export default class Register extends React.Component{
 
     render(){
         
+        const {navigate} = this.props.navigation
         return(
-            <View style={{height:"50%"}}>
-                <Image source ={require('../images/image.png')}
-                    style={{width:"50%",height:"40%"}}
-                />
+            <View style={{ flex: 1, justifyContent: "center", alignItems: "center" }}>
+               <Image source={require('../images/P.png')}
+                style={{ width: '60%', height: "30%", marginTop: -111}}
+                
+               />
                 <Text
                  style={{
-                     fontSize:30,
+                     fontSize:20,
                      fontFamily:"SemiBold",
                      alignSelf:"center",
                  }}
-                >Welcome to PARFINDER</Text>
+                >WELCOME TO PARK FINDER</Text>
 
                 <Text
                 style={{
                     fontFamily:"SemiBold",
                     marginHorizontal:55,
                     textAlign:'center',
-                    marginTop:5,
+                    marginTop:50,
                     opacity:0.4
                 }}
                 >
-                    Amet minim mollit non deserunt ullamco est sit aliqua dolor do amet sint. Velit officia consequat duis enim velit mollit.
+                    ENTER YOUR INFORMATION HERE
                 </Text>
 
                 <View style={{
                     flexDirection:"row",
                     alignItems:"center",
-                    marginHorizontal:55,
-                    borderWidth:2,
-                    marginTop:50,
+                    marginHorizontal:15,
+                    borderWidth:3,
+                    marginTop:10,
                     paddingHorizontal:10,
-                    borderColor:"#1bacc5",
+                    borderColor:"#404040",
                     borderRadius:23,
-                    paddingVertical:2
+                    paddingVertical:9,
+                    width: '80%'
                 }}>
+                    <Icon name="user" color="#FF0000" size={24} />
                   
                     <TextInput 
-                        placeholder="Email"
-                        placeholderTextColor="#1bacc5"
-                        style={{paddingHorizontal:10}}
+                        placeholder="USER NAME"
+                        placeholderTextColor="#FF0000"
+                        style={{paddingHorizontal:10, borderBottomWidth: 1, width: '90%', borderBottomColor: ""}}
                     />
 
                     
@@ -55,61 +59,117 @@ export default class Register extends React.Component{
                 <View style={{
                     flexDirection:"row",
                     alignItems:"center",
-                    marginHorizontal:55,
-                    borderWidth:2,
-                    marginTop:15,
+                    marginHorizontal:15,
+                    borderWidth:3,
+                    marginTop:10,
                     paddingHorizontal:10,
-                    borderColor:"#1bacc5",
+                    borderColor:"#404040",
                     borderRadius:23,
-                    paddingVertical:2
+                    paddingVertical:9,
+                    width: '80%'
                 }}>
+                    <Icon name="mail" color="#FF0000" size={24} />
                    
                    <TextInput 
                         secureTextEntry
-                        placeholder="Password"
-                        placeholderTextColor="#1bacc5"
-                        style={{paddingHorizontal:10}}
+                        placeholder=" EMAIL"
+                        placeholderTextColor="#FF0000"
+                        style={{paddingHorizontal:10, borderBottomWidth: 1, width: '90%', borderBottomColor: ""}}
                     />
-
-
-                    
 
                 </View>
                 <View style={{
                     flexDirection:"row",
                     alignItems:"center",
-                    marginHorizontal:55,
-                    borderWidth:2,
-                    marginTop:15,
+                    marginHorizontal:15,
+                    borderWidth:3,
+                    marginTop:10,
                     paddingHorizontal:10,
-                    borderColor:"#1bacc5",
+                    borderColor:"#404040",
                     borderRadius:23,
-                    paddingVertical:2
+                    paddingVertical:9,
+                    width: '80%'
                 }}>
+                    <Icon name="phone" color="#FF0000" size={24} />
                    
                    <TextInput 
                         secureTextEntry
-                        placeholder="Confirm Password"
-                        placeholderTextColor="#1bacc5"
-                        style={{paddingHorizontal:10}}
+                        placeholder=" PHONE NUMBER"
+                        placeholderTextColor="#FF0000"
+                        style={{paddingHorizontal:10, borderBottomWidth: 1, width: '90%', borderBottomColor: ""}}
                     />
                     
 
                 </View>
+
+                <View style={{
+                    flexDirection:"row",
+                    alignItems:"center",
+                    marginHorizontal:15,
+                    borderWidth:3,
+                    marginTop:10,
+                    paddingHorizontal:10,
+                    borderColor:"#404040",
+                    borderRadius:23,
+                    paddingVertical:9,
+                    width: '80%'
+                }}>
+                    <Icon name="lock" color="#FF0000" size={24} />
+                   
+                   <TextInput 
+                        secureTextEntry
+                        placeholder="PASSWORD "
+                        placeholderTextColor="#FF0000"
+                        style={{paddingHorizontal:10, borderBottomWidth: 1, width: '90%', borderBottomColor: ""}}
+                    />
+                    
+
+                </View>
+
+                <View style={{
+                    flexDirection:"row",
+                    alignItems:"center",
+                    marginHorizontal:15,
+                    borderWidth:3,
+                    marginTop:10,
+                    paddingHorizontal:10,
+                    borderColor:"#404040",
+                    borderRadius:23,
+                    paddingVertical:9,
+                    width: '80%'
+                }}>
+                   <Icon name="lock" color="#FF0000" size={24} />
+                   <TextInput 
+                        secureTextEntry
+                        placeholder="RETYPE PASSWORD"
+                        placeholderTextColor="#FF0000"
+                        style={{paddingHorizontal:10, borderBottomWidth: 1, width: '90%', borderBottomColor: ""}}
+                    />
+                    
+
+                </View>
+
+               
+
+
 
                 <View style={{
                     marginHorizontal:55,
                     alignItems:"center",
                     justifyContent:"center",
                     marginTop:30,
-                    backgroundColor:"#1bacc5",
+                    backgroundColor:"#FF0000",
                     paddingVertical:10,
-                    borderRadius:23
+                    borderRadius:23,
+                    width: '80%'
                 }}>
+                    
                     <Text style={{
                         color:"white",
-                        fontFamily:"SemiBold"
-                    }}>Register</Text>
+                        fontFamily:"SemiBold",
+                        width: "100%",
+                        textAlign: 'center'
+                    }}>CONFIRM</Text>
                 </View>
               
             </View>
